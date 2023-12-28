4ae2ab8a6f472f44920388c375ae118362c2e251
Arahabica <r.sasage@gmail.com>
2023-12-28 15:09:43 +0900

    chore: add test

--- src/test/lib/ImageSizeScale.test.ts
+++ src/test/lib/ImageSizeScale.test.ts
@@ -2,14 +2,31 @@ import { it, describe, expect } from 'vitest'
 import { ImageSizeScale } from '../../lib/ImageSizeScale'
 
 describe('ImageSizeScale', () => {
-  it('size', async () => {
-    const imageUrl = "https://via.placeholder.com/400x100/79b74a/fff.jpg/?text=Larkdown"
-    const res = await fetch(imageUrl)
-    const arrayBuffer = await res.arrayBuffer()
-    const buffer = Buffer.from(arrayBuffer)
+  describe('size', () => {
+    it('jpg', async () => {
+      const imageUrl = "https://via.placeholder.com/400x100/79b74a/fff.jpg/?text=Larkdown"
+      const res = await fetch(imageUrl)
+      const arrayBuffer = await res.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
 
-    const scale = new ImageSizeScale()
-    const size = scale.size(buffer)
-    expect(size).toEqual({ width: 400, height: 100 })
+      const scale = new ImageSizeScale()
+      const size = scale.size(buffer)
+      expect(size).toEqual({ width: 400, height: 100 })
+    })
+    it('pdf', async () => {
+      const imageUrl = "https://www.africau.edu/images/default/sample.pdf"
+      const res = await fetch(imageUrl)
+      const arrayBuffer = await res.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
+
+      let error: Error | undefined = undefined
+      const scale = new ImageSizeScale()
+      try {
+        scale.size(buffer)
+      } catch (e) {
+        error = e as Error
+      }
+      expect(error).toBeTruthy()
+    })
   })
 })
