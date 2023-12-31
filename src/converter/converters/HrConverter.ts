ac67ad3ad65fd9700d7352d8e05277ca774fb9e9
Arahabica <r.sasage@gmail.com>
2024-01-01 03:59:28 +0900

    chore: modify HrConverter

--- src/converter/converters/HrConverter.ts
+++ src/converter/converters/HrConverter.ts
@@ -3,36 +3,11 @@ import { FlexComponent } from "@line/bot-sdk"
 import { FlexConverter } from "../../types"
 
 export class HrConverter implements FlexConverter {
-  private readonly borderColor = "#e3e5ed"
-  private readonly borderHeight = 2
-  private readonly margin = 16
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async convert(_: Tokens.Hr): Promise<FlexComponent[]> {
-    const { borderColor, borderHeight, margin } = this
-    const component: FlexComponent = {
-      type: 'box',
-      layout: 'vertical',
-      margin: `${margin}px`,
-      contents: [
-        {
-          type: 'box',
-          layout: 'horizontal',
-          backgroundColor: borderColor,
-          width: '100%',
-          height: `${borderHeight}px`,
-          cornerRadius: `${Math.floor(borderHeight * 0.5)}px`,
-          contents: [],
-        },
-        {
-          type: 'box',
-          layout: 'horizontal',
-          width: '100%',
-          height: `${margin}px`,
-          contents: [],
-        },
-      ]
-    }
-    return [component]
+    return [{
+      type: 'separator',
+      margin: 'md'
+    }]
   }
 }
