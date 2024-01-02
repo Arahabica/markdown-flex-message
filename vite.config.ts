fa211f8e70451a0dd7fb9e2d8b977a11389e226c
Arahabica <r.sasage@gmail.com>
2024-01-03 01:10:11 +0900

    feature: coverage

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    coverage: {
+      all: true,
+      exclude: [
+        'src/**/*.d.ts',
+        'src/test/**/*',
+        'src/types.ts'
+      ]
+    }
   }
 })
